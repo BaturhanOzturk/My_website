7b351bd2b728ec7555c0cf314de7ac453212327d
Desmond <153235303+BaturhanOzturk@users.noreply.github.com>
2026-01-17 22:33:35 +0300

    v0.03

--- src/Companents/About.jsx
+++ src/Companents/About.jsx
@@ -25,8 +25,8 @@ function About() {
             </div>
     
             <div className='mt-4'>
-              <h2 className='font-medium'>Vizyonum:</h2>
-              <p>Teknoloji dünyasında iz bırakan, insanların hayatını kolaylaştıran çözümler geliştiren bir yazılım uzmanı olmak.</p>
+              <h2 className='font-medium'>My Vision:</h2>
+              <p>To be a software expert who makes a mark in the world of technology and develops solutions that simplify people's lives.</p>
             </div>
           </div>
         </div>
@@ -41,24 +41,24 @@ function About() {
                      alt="Baturhan Öztürk" />
             </div>
             <h1 className='mt-3 cardtitle text-center text-xl md:text-2xl'>Baturhan Öztürk</h1>
-            <p className='cardtext p-4 text-sm md:text-base text-center md:text-left'>Ben Baturhan Öztürk. Yazılım alanında kendimi geliştirmeye devam ediyorum. Özellikle web geliştirme alanında yoğunlaştım. Bunun yanı sıra, yer aldığım robotik takımı sayesinde robotik kodlama konusunda da deneyim kazandım. Şuanda odağımı yapay zeka alanına çevirdim. Hedefim, ileri düzey yazılım projelerinde yer almak.</p>
+            <p className='cardtext p-4 text-sm md:text-base text-center md:text-left'>My name is Baturhan Öztürk. I continue to develop myself in the field of software. I have focused particularly on web development. In addition, I have gained experience in robotic coding through my involvement in a robotics team. I have now shifted my focus to the field of artificial intelligence. My goal is to participate in advanced software projects.</p>
         </div>
 
         {/* Sağ Kart*/}
 
         <div className='shadow-lg rounded-md w-full md:w-auto bg-gray-100 secondcard md:ml-30 max-w-sm p-4'>
-          <h1 className='text-xl font-bold text-center mb-3'>İlgi Alanları</h1>
+          <h1 className='text-xl font-bold text-center mb-3'>Interests</h1>
   
           <ul className='list-disc pl-5 space-y-2 mb-4'>
-            <li>Yazılım Geliştirme</li>
-            <li>Yapay Zeka & Veri Bilimi</li>
-            <li>Robotik Kodlama</li>
-            <li>Web Tasarım</li>
+            <li>Software Development</li>
+            <li>Artificial Intelligence & Data Science</li>
+            <li>Robotics Coding</li>
+            <li>Web Design</li>
           </ul>
   
           <div className='mt-6 border-t pt-4'>
             <p className='text-center italic text-sm font-medium'>
-              "Eğer her şey tahmin edilebilirse o zaman hiç bilgi yoktur. Eğer işin içinde rassallık varsa, o zaman her şey hakkında bilgiye ihtiyacınız var."
+              “The real problem is not whether machines think but whether people do.”
             </p>
           </div>
         </div>
